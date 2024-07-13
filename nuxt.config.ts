8cf9a96e90ae06c7a72ba7e57cfb7a124bdb863b
Sabih S <67878058+kleenkanteen@users.noreply.github.com>
2024-07-13 12:09:09 -0400

    nuxt starter + docs update (#53)
    + add tailwind and replace eslint with biome cus faster

---
+++ nuxt.config.ts
@@ -0,0 +1,28 @@
+import { defineNuxtConfig } from 'nuxt/config'
+
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  // Nuxt 4 directory structure and features
+  // https://nuxt.com/docs/getting-started/upgrade#testing-nuxt-4
+  future: { compatibilityVersion: 4 },
+  // Nuxt Modules
+  // https://nuxt.com/modules
+  modules: [
+    '@nuxthub/core',
+    '@nuxtjs/tailwindcss'
+  ],
+  hub: {
+    database: true,
+    kv: true,
+    blob: true,
+    cache: true,
+  },
+  nitro: {
+    experimental: {
+      // Enable Server API documentation within NuxtHub
+      openAPI: true
+    }
+  },
+  // Development
+  devtools: { enabled: true },
+})
