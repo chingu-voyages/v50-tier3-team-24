74138b3eef4f2491ccbb229766ece5a9c36fac60
David Eastmond <davebuck1981@gmail.com>
2024-07-15 17:32:34 -0400

    create interface for the annotedoc

--- types/annote-document/annote-document.ts
+++ types/annote-document/annote-document.ts
@@ -1,11 +1,13 @@
 export interface AnnoteDocument {
   id: string;
-  userId: string;
-  path: string;
+  userId: string; // This is the foreign key that refers to the user
+  slug: string; // Refers to the document slug
   title: string;
   body: string;
+  description: string;
   createdAt: Date;
   updatedAt: Date;
   sourceUrl?: string;
+  imageUrl?: string;
   visibility: "private" | "public";
 }
