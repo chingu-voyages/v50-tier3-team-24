be2c795b1b24b07d7e8d75e1b08c4384b9f24bcd
David Eastmond <davebuck1981@gmail.com>
2024-08-02 14:35:05 -0400

    create stickyDbClient

--- server/utils/database/sticky-db-client/sticky-db-client.ts
+++ server/utils/database/sticky-db-client/sticky-db-client.ts
@@ -1,11 +1,21 @@
-import { Sticky } from "~/types/sticky/sticky-types";
+import { LinkSticky, Sticky, VideoSticky } from "~/types/sticky/sticky-types";
 import { BaseDbClient } from "../base-db-client";
 
 export class StickyDbClient extends BaseDbClient {
   constructor() {
     super("sticky");
   }
-  public async insertSticky(sticky: Partial<Sticky>): Promise<void> {
-    console.log;
+
+  public async insertSticky(
+    user_id: string,
+    stickyData: Partial<Sticky>
+  ): Promise<Sticky | VideoSticky | LinkSticky | null> {
+    const { error, data } = await this.client
+      .from(this.TABLE_NAME)
+      .insert({ ...stickyData, user_id })
+      .select();
+
+    if (error) throw new Error(error.message);
+    return data?.[0] || null;
   }
 }
