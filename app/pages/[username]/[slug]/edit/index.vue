f5dfe3f7f7376acbced808839e7d9e94db08a094
David Eastmond <davebuck1981@gmail.com>
2024-08-02 21:48:09 -0400

    Edit annotation doc refactor

--- app/pages/[username]/[slug]/edit/index.vue
+++ app/pages/[username]/[slug]/edit/index.vue
@@ -15,21 +15,61 @@
             <Icon name="mdi:trash-can-outline" />
           </div>
         </div>
-        <ClientOnly>
-          <EditorComponent :onEditorReady="handleEditorReady" />
-        </ClientOnly>
+        <div class="flex bg-gray-100 border border-gray-300 rounded p-5 min-h-[300px] my-5">
+          <ClientOnly>
+            <EditorComponent :onEditorReady="handleEditorReady" :onMarkerInserted="handleMarkerInserted" />
+          </ClientOnly>
+          <div>
+            <Transition>
+              <div v-if="isInsertingNewAnnotation" id="new-sticky-container p-2">
+                <!-- This is for new annotation stickes -->
+                <StickyNote 
+                  :isNew="isInsertingNewAnnotation" 
+                  :color="newStickyData?.color" 
+                  :pinNumber="newStickyData?.pinNumber" 
+                  :documentId="newStickyData?.documentId" 
+                  :title="newStickyData?.title"
+                  :onUpdateCreate="handleUpdateCreateSticky"
+                  :onCancel="handleCloseOutSticky"
+                />
+              </div>
+            </Transition>
+            <div class="flex">
+              <StickyNote 
+                v-for="sticky in stickies" :key="sticky.sticky_id"
+                :stickyData="sticky" 
+                :color="sticky.color" 
+                :pinNumber="sticky.anchor" 
+                :documentId="sticky.document_id" 
+                :readonly="true"
+                :onDelete="handleDeleteSticky"
+              />
+            </div>
+           
+          </div>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import type { Sticky } from 'types/sticky/sticky-types';
+import type { ActionType } from '../../../../../types/sticky/action-type/action-type';
+import type { StickyCreateActionData } from '../../../../../types/sticky/sticky-create-action-data/sticky-create-action-data';
+import type { AnnoteOnMarkerInsertedData } from '../../../../utils/annote-marker/definitions/types';
 const annoteDocument = ref<AnnoteDocument | null>(null);
 const documentTitle = ref("");
 const editorController = ref<CustomEditorJs | null>(null);
 
 const initialDocumentTitle = ref(""); // The original title when the document loads. This will not change.
 const route = useRoute();
+
+const isInsertingNewAnnotation = ref(false);
+const newStickyData = ref<{ pinNumber: number; color: string; title: string, documentId: string } | null>(null);
+
+const stickies = ref<Sticky[]>([]);
+
 const { id } = route.query;
 
 async function handleDocumentTitleBlur() {
@@ -59,7 +99,6 @@ async function handleDocumentTitleBlur() {
     },
   });
 }
-
 async function handleDocumentBodyBlur() {
   // This function handles when the user clicks out of the body textarea
   // It should send a patch request to the server to update the document body
@@ -70,6 +109,24 @@ async function handleDocumentBodyBlur() {
   // });
 }
 
+async function patchAnnoteDocumentBlocks(): Promise<AnnoteDocument> {
+  const blockData = await editorController.value?.save();
+
+  const { data: apiResponse } = await useFetch<ApiResponse<AnnoteDocument>>(
+    `/api/annote_documents/${id}`,
+    {
+      method: "PATCH",
+      body: { blocks: blockData?.blocks },
+    }
+  );
+  return apiResponse.value?.data!;
+}
+
+async function fetchStickies (): Promise<Sticky[]> {
+  const { data: apiResponse } = await useFetch<ApiResponse<Sticky[]>>(`/api/annote_documents/${id}/sticky`);
+  return apiResponse.value?.data!;
+}
+
 if (id) {
   const { data: apiResponse } = await useFetch<ApiResponse<AnnoteDocument>>(
     `/api/annote_documents/${id}`
@@ -77,14 +134,87 @@ if (id) {
   annoteDocument.value = apiResponse.value?.data!;
   initialDocumentTitle.value = annoteDocument.value.title;
   documentTitle.value = annoteDocument.value.title;
+
+  stickies.value = await fetchStickies();
+}
+
+function handleMarkerInserted (data?: AnnoteOnMarkerInsertedData) {
+  if (!data) {
+    console.warn("No data was returned. Aborting");
+    return;
+  }
+  isInsertingNewAnnotation.value = true;
+  const { pinNumber, color, text } = data!;
+  newStickyData.value = { pinNumber, color, title: text || "", documentId: annoteDocument.value?.document_id! };
 }
 
 function handleEditorReady(editor: CustomEditorJs) {
   editorController.value = editor;
   editor.isReady.then(() => {
     editor.render({
-      blocks: annoteDocument.value?.blocks as any,
+      blocks: annoteDocument.value?.blocks as any,  
     });
   });
 }
+
+async function handleUpdateCreateSticky(action: ActionType, values: StickyCreateActionData) {
+  if (action === "create") {
+    // This method needs to also patch the annote document
+    // Create a new sticky note
+    const { document_id, title, body, anchor, color, sticky_type } = values;
+    await useFetch<ApiResponse<Sticky>>(
+      `/api/sticky`,
+      {
+        method: "POST",
+        body: {
+          document_id,
+          title,
+          body,
+          color,
+          anchor,
+          sticky_type,
+        },
+      }
+    );
+    annoteDocument.value = await patchAnnoteDocumentBlocks();
+    stickies.value = await fetchStickies();
+    console.log("163", stickies.value);
+  }
+
+  isInsertingNewAnnotation.value = false;
+  newStickyData.value = null;
+}
+
+function handleCloseOutSticky () {
+  isInsertingNewAnnotation.value = false;
+  newStickyData.value = null;
+  // This should re-render the editor to the last fetched-from-API state, undoing the new marker. TODO: It's hacky
+  editorController.value?.render({
+    blocks: annoteDocument.value?.blocks as any,
+  })
+}
+
+async function handleDeleteSticky (sticky_id: string) {
+  console.log("hi")
+  await useFetch<ApiResponse<Sticky>>(
+    `/api/sticky/${sticky_id}`,
+    {
+      method: "DELETE",
+    }
+  );
+  annoteDocument.value = await patchAnnoteDocumentBlocks();
+  stickies.value = await fetchStickies();
+}
 </script>
+
+<style scoped>
+.v-enter-active,
+.v-leave-active {
+  transition: opacity 0.5s ease;
+}
+
+.v-enter-from,
+.v-leave-to {
+  opacity: 0;
+}
+</style>
