b81781e599877a657eb41443eddb73ac08de9a3e
Tandid Alam <tandid.alam@gmail.com>
2024-08-06 23:59:28 -0400

    added Icons

--- app/components/Navbar.vue
+++ app/components/Navbar.vue
@@ -2,36 +2,51 @@
   <nav class="p-4 text-black border-b-2 border-gray-100">
     <ul class="flex justify-between p-0 m-0 list-none">
       <li>
-        <NuxtLink to="/" class="text-3xl no-underline font-gloria">
-          📝 Annote
+        <NuxtLink to="/" class="flex items-center text-3xl no-underline">
+          <img :src="documentIcon" alt="Icon" class="w-8 h-8 mr-2" />
+          Annote
         </NuxtLink>
       </li>
 
       <div class="flex items-center space-x-6 text-xl">
         <li>
-          <NuxtLink to="/about" class="no-underline font-gloria"
-            >About</NuxtLink
+          <NuxtLink to="/about" class="flex items-center no-underline">
+            <img
+              :src="homeIcon"
+              alt="Icon"
+              class="w-8 h-8 mr-2"
+            />About</NuxtLink
           >
         </li>
         <li>
-          <NuxtLink to="/library" class="no-underline font-gloria"
-            >Library</NuxtLink
+          <NuxtLink to="/library" class="flex items-center no-underline">
+            <img
+              :src="bookIcon"
+              alt="Icon"
+              class="w-8 h-8 mr-2"
+            />Library</NuxtLink
           >
         </li>
         <li>
-          <NuxtLink to="/new" class="no-underline font-gloria">New</NuxtLink>
+          <NuxtLink to="/new" class="flex items-center no-underline">
+            <img
+              :src="addCircleIcon"
+              alt="Icon"
+              class="w-8 h-8 mr-2"
+            />New</NuxtLink
+          >
         </li>
 
         <li v-if="user">
-          <span v-if="currentUser">
-            Welcome, {{ currentUser.username }}!
-          </span>
-          <button @click="handleLogout" class="no-underline font-gloria">
+          <!-- <span v-if="currentUser"> Welcome, {{ currentUser.username }}! </span> -->
+
+          <button @click="handleLogout" class="flex items-center no-underline">
+            <img :src="accountIcon" alt="Icon" class="w-8 h-8 mr-2" />
             Logout
           </button>
         </li>
         <li v-else>
-          <NuxtLink to="/login" class="no-underline font-gloria"
+          <NuxtLink to="/login" class="flex items-center no-underline"
             >Sign In</NuxtLink
           >
         </li>
@@ -41,6 +56,11 @@
 </template>
 
 <script setup>
+import accountIcon from "@/public/assets/icons/account_circle.svg";
+import addCircleIcon from "@/public/assets/icons/add_circle.svg";
+import documentIcon from "@/public/assets/icons/edit_document.svg";
+import homeIcon from "@/public/assets/icons/help_clinic.svg";
+import bookIcon from "@/public/assets/icons/library_books.svg";
 import { useAuth } from "../composables/useAuth";
 
 const user = useSupabaseUser();
