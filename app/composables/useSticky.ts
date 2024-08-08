72e657e0ad9c1af1d20c7278367f1390d6186185
David Eastmond <davebuck1981@gmail.com>
2024-08-08 11:26:20 -0400

    added comment on composable

--- app/composables/useSticky.ts
+++ app/composables/useSticky.ts
@@ -1,4 +1,9 @@
 export function useSticky() {
+  /**
+   * This composable fetches all of the stickies associated with a specific AnnoteDocument
+   * @param id the AnnoteDocument id to which the sticky belongs
+   * @returns
+   */
   async function fetchStickies(id: string): Promise<Sticky[]> {
     const { data: apiResponse } = await useFetch<ApiResponse<Sticky[]>>(
       `/api/annote_documents/${id}/sticky`
