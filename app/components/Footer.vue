ea0f857c7e53dbf39a75f43101e36d1f899be356
Tandid Alam <tandid.alam@gmail.com>
2024-08-09 14:03:54 -0400

    added basic footer

---
+++ app/components/Footer.vue
@@ -0,0 +1,39 @@
+<template>
+  <footer class="py-8 text-gray-600 bg-gray-100">
+    <div class="container px-4 mx-auto">
+      <div class="flex flex-wrap justify-between">
+        <div class="w-full mb-4 md:w-1/4 md:mb-0">
+          <h3 class="mb-2 text-lg font-semibold">Annote</h3>
+          <p>Streamline your note-taking and boost productivity</p>
+        </div>
+        <div class="w-full mb-4 md:w-1/4 md:mb-0">
+          <h4 class="mb-2 font-semibold text-md">Quick Links</h4>
+          <ul>
+            <li>
+              <NuxtLink to="/" class="hover:text-gray-900">Home</NuxtLink>
+            </li>
+            <li>
+              <NuxtLink to="/about" class="hover:text-gray-900">About</NuxtLink>
+            </li>
+            <li>
+              <NuxtLink to="/library" class="hover:text-gray-900"
+                >Library</NuxtLink
+              >
+            </li>
+          </ul>
+        </div>
+        <div class="w-full mb-4 md:w-1/4 md:mb-0">
+          <h4 class="mb-2 font-semibold text-md">Contact</h4>
+          <p>support@annote.com</p>
+        </div>
+      </div>
+      <div class="mt-8 text-center">
+        <p>&copy; 2023 Annote. All rights reserved.</p>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<script setup>
+// Any necessary setup logic can go here
+</script>
