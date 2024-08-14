0b5235d79c628fa653addd030837da97884bbb27
David Eastmond <davebuck1981@gmail.com>
2024-08-14 16:25:55 -0400

    Feature/enhancements loading spinners (#124)
    * install vue3-spinners
    
    * factor out SpinnerButton. Added it to login and signup
    
    * remove console.log
    
    * new button on the library page navigates to the new page
    
    * Spinner on library.vue
    
    * adjust the menu
    
    * add some busy spinners

---
+++ app/components/SpinnerButton.vue
@@ -0,0 +1,26 @@
+<template>
+  <button
+    type="submit"
+    class="w-full px-4 py-2 text-white bg-blue-500 rounded-md hover:bg-blue-600"
+    :disabled="isBusy"
+  >
+    <div class="flex gap-x-2 justify-center">
+      <!-- TODO: We can use a conditional slot to render a custom spinner -->
+      <VueSpinner v-if="props.isBusy" size="20" color="white" />
+      <p>
+        {{ props.title }}
+      </p>
+    </div>
+  </button>
+</template>
+
+<script lang="ts" setup>
+import { VueSpinner } from 'vue3-spinners';
+interface SpinnerButtonProps {
+  title: string;
+  isBusy?: boolean;
+}
+
+const props = defineProps<SpinnerButtonProps>();
+
+</script>
