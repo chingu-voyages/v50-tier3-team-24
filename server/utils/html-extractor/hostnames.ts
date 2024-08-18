7ec1f230bc1e69015b0e13ff1f0f1486e9887776
David Eastmond <davebuck1981@gmail.com>
2024-08-18 18:14:09 -0400

    refactor hostnames so key is the domain

--- server/utils/html-extractor/hostnames.ts
+++ server/utils/html-extractor/hostnames.ts
@@ -2,28 +2,25 @@
 
 // The intent of this shouldn't be something that's exhaustive. It's just a sample of customization.
 const HOST_NAMES: Record<string, string[]> = {
-  article: ["serpapi.com"],
-  ".article-content": ["css-tricks.com"],
-  "[data-articlebody]": ["timesofindia.indiatimes.com"],
-  "#main-content": ["www.timesofisrael.com"],
-  ".body": ["docs.python.org"],
-  ".container article": ["constitutioncenter.org"],
+  "serpapi.com": ["article"],
+  "css-tricks.com": [".article-content"],
+  "timesofindia.indiatimes.com": ["[data-articlebody]"],
+  "www.timesofisrael.com": ["#main-content"],
+  "docs.python.org": [".body"],
+  "constitutioncenter.org": [".container article"],
+  "www.japantimes.co.jp": ["#jtarticle"],
+  "www.nbcnews.com": [".article-body"],
 };
 
 export const getSelectorByHostName = (hostName: string): string[] => {
-  const selectors: string[] = [];
-  for (const selector in HOST_NAMES) {
-    if (HOST_NAMES[selector].includes(hostName)) {
-      selectors.push(selector);
-    }
-  }
+  const selectors = HOST_NAMES[hostName];
 
-  if (selectors.length > 0) {
+  if (selectors) {
     return selectors;
   }
 
   console.warn(
-    `No selector found for ${hostName}, defaulting to selector 'main'`
+    `No selectors found for ${hostName}, defaulting to selector 'main'`
   );
   return ["main"]; // We can probably do some default selector here
 };
