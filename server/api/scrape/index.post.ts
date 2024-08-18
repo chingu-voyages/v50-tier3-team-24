9f07cf80bef76970775d556dc0e05a3609cfe25d
David Eastmond <davebuck1981@gmail.com>
2024-08-18 10:09:16 -0400

    tweak endpoint

--- server/api/scrape/index.post.ts
+++ server/api/scrape/index.post.ts
@@ -1,3 +1,5 @@
+import { HtmlToBlockMapper } from "#imports";
+import { getSelectorByHostName } from "~/server/utils/html-extractor/hostnames";
 import { HtmlExtractor } from "~/server/utils/html-extractor/html-extractor";
 
 export default defineEventHandler(async (event) => {
@@ -6,12 +8,16 @@ export default defineEventHandler(async (event) => {
   try {
     // Scrape the URL
     const content = await WebScraper.scrape(url);
+    const hostName = new URL(url).hostname;
 
     // Extract the article content, covert it to an array of ArticleContent
     // Map the ArticleContent to EditorJS blocks and return a response
+
     return {
       status: "ok",
-      data: new HtmlToBlockMapper().map(new HtmlExtractor(content).extract()),
+      data: new HtmlToBlockMapper(hostName).map(
+        new HtmlExtractor(content).extract(getSelectorByHostName(hostName))
+      ),
     };
   } catch (error: any) {
     return {
