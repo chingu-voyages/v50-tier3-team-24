6811118f2bd62e8a029ca71ff7c8a9608aee011f
David Eastmond <davebuck1981@gmail.com>
2024-08-18 10:06:20 -0400

    refactor some logic

--- server/utils/html-to-block-mapper/html-to-block-mapper.ts
+++ server/utils/html-to-block-mapper/html-to-block-mapper.ts
@@ -7,38 +7,48 @@ import { ArticleContent, HTMLTextElement } from "../html-extractor/definitions";
 
 // This class maps the array of ArticleContent as outputted from the html-extract EditorJS blocks
 export class HtmlToBlockMapper {
-  // This 'directory' basically tells the mapper how to map each HTML element to an EditorJS block
-  private directory: Record<HTMLTextElement, Function> = {
-    h1: this.mapHeader,
-    h2: this.mapHeader,
-    h3: this.mapHeader,
-    h4: this.mapHeader,
-    h5: this.mapHeader,
-    h6: this.mapHeader,
-    p: this.mapParagraph,
-    ul: this.mapList,
-    ol: this.mapList,
-    div: () => null,
-    li: () => null,
-  };
+  private hostName: string;
 
-  public map(articleContent: ArticleContent[]): EditorJsBlock[] {
+  constructor(hostName: string) {
+    this.hostName = hostName;
+    console.info("hostName:", this.hostName);
+  }
+
+  public map(
+    articleContent: ArticleContent[],
+    hostName?: string
+  ): EditorJsBlock[] {
+    // This 'directory' basically tells the mapper how to map each HTML element to an EditorJS block
+    const directory: Record<HTMLTextElement, Function> = {
+      h1: this.mapHeader,
+      h2: this.mapHeader,
+      h3: this.mapHeader,
+      h4: this.mapHeader,
+      h5: this.mapHeader,
+      h6: this.mapHeader,
+      p: this.mapParagraph,
+      ul: this.mapList,
+      ol: this.mapList,
+      div: this.getCustomDivMapper(),
+      li: () => null,
+    };
+
     const blocks: EditorJsBlock[] = [];
+
     articleContent.forEach((content, index) => {
       if (content.contentType === "ul" || content.contentType === "ol") {
         // HTML List elements need to be treated slightly differently given that the articleContent comes to this function as flattened
         // Theoretically how I've parsed the articleContent, <li> elements should directly follow <ul> or <ol> elements.
         blocks.push(
-          this.directory[content.contentType](
+          directory[content.contentType](
             content,
             articleContent.slice(index + 1) // Splice the array to get the next elements in the list. They will be used to create <li> if they exist
           )
         );
       } else {
-        blocks.push(this.directory[content.contentType](content));
+        blocks.push(directory[content.contentType](content));
       }
     });
-
     return blocks.filter((block) => block !== null);
   }
 
@@ -80,6 +90,15 @@ export class HtmlToBlockMapper {
         items: listItems,
       },
     };
+  }
+
+  private getCustomDivMapper(): Function {
+    switch (this.hostName) {
+      case "timesofindia.indiatimes.com":
+        return this.mapParagraph;
+      default:
+        return () => null;
+    }
   }
 }
 
