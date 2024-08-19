560450b5076d80c9ee6ecf7a6f99e25a06308cf6
David Eastmond <davebuck1981@gmail.com>
2024-08-19 07:52:33 -0400

    secure the web-scrape route and add validator

--- server/api/scrape/index.post.ts
+++ server/api/scrape/index.post.ts
@@ -1,9 +1,38 @@
 import { HtmlToBlockMapper } from "#imports";
+import { serverSupabaseUser } from "#supabase/server";
 import { getSelectorsByHostName } from "~/server/utils/html-extractor/hostnames";
 import { HtmlExtractor } from "~/server/utils/html-extractor/html-extractor";
+import { webScraperValidator } from "~/server/utils/validators/web-scraper/web-scraper-validator";
 
 export default defineEventHandler(async (event) => {
+  const user = await serverSupabaseUser(event);
+  if (!user) {
+    setResponseStatus(event, 401);
+    return {
+      status: "fail",
+      error: createError({
+        statusCode: 401,
+        statusMessage: "Unauthorized",
+      }),
+    };
+  }
+
   const requestBody = await readBody<{ url: string }>(event);
+
+  try {
+    // Validate the request
+    await webScraperValidator.validate(requestBody, { abortEarly: false });
+  } catch (error: any) {
+    setResponseStatus(event, 400);
+    return {
+      status: "fail",
+      error: createError({
+        statusCode: 400,
+        statusMessage: error.message,
+      }),
+    };
+  }
+
   const { url } = requestBody;
   try {
     // Scrape the URL
