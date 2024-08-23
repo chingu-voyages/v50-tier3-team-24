b4342ef598ebfa2f58363815489ec98ea5c25650
David Eastmond <davebuck1981@gmail.com>
2024-08-23 16:40:42 -0400

    css tweak to spinner button

--- app/components/SpinnerButton.vue
+++ app/components/SpinnerButton.vue
@@ -1,7 +1,7 @@
 <template>
   <button
     type="submit"
-    class="w-full px-4 py-2 text-white bg-blue-500 rounded-md hover:bg-blue-600"
+    class="px-4 py-2 text-white bg-blue-500 rounded-md hover:bg-blue-600"
     :disabled="isBusy"
   >
     <div class="flex gap-x-2 justify-center">
