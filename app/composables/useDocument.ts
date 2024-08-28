be91d69dc3751d5487f071b1a5b3ab4298fa0f4b
David Eastmond <davebuck1981@gmail.com>
2024-08-28 05:57:45 -0400

    isMarkerInViewport hook

--- app/composables/useDocument.ts
+++ app/composables/useDocument.ts
@@ -6,5 +6,19 @@ export function useDocument() {
 
     return res.data.value;
   }
-  return { deleteDocument };
+  function isMarkerInViewPort(htmlElement: HTMLElement): boolean {
+    // Reference: https://jacobnarayan.com/blogs/how-to-tell-if-an-html-element-is-in-the-viewport
+    const rect = htmlElement.getBoundingClientRect();
+
+    const isinview =
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <=
+        (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth);
+
+    // returns true or false based on whether or not the element is in viewport
+    return isinview;
+  }
+  return { deleteDocument, isMarkerInViewPort };
 }
