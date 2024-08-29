1374e2d2d9573171d1fec394cbdbc1af29636583
David Eastmond <davebuck1981@gmail.com>
2024-08-29 14:49:02 -0400

    some cleanup

--- app/pages/[username]/[slug]/edit/index.vue
+++ app/pages/[username]/[slug]/edit/index.vue
@@ -1,13 +1,8 @@
 <template>
   <div>
     <div class="flex">
-      <input
-        type="text"
-        v-model="documentTitle"
-        @blur="handleDocumentTitleBlur"
-        class="w-full p-2 border border-gray-300 rounded font-verdana"
-        placeholder="Title"
-      />
+      <input type="text" v-model="documentTitle" @blur="handleDocumentTitleBlur"
+        class="w-full p-2 border border-gray-300 rounded font-verdana" placeholder="Title" />
       <div class="flex items-center gap-2 ml-2">
         <button class="hover:custom-green">
           <Icon name="mdi:check" />
@@ -19,62 +14,51 @@
     </div>
     <div class="flex bg-gray-100 border border-gray-300 rounded p-5 min-h-[300px] my-5">
       <ClientOnly>
-        <EditorComponent :onEditorReady="handleEditorReady" :onMarkerInserted="handleMarkerInserted" :onMarkerDeleted="handleDeleteMarker" :onLostFocus="handleEditorLostFocus" />
+        <div class="min-w-[50%]">
+          <EditorComponent :onEditorReady="handleEditorReady" :onMarkerInserted="handleMarkerInserted"
+            :onMarkerDeleted="handleDeleteMarker" :onLostFocus="handleEditorLostFocus" />
+        </div>
       </ClientOnly>
       <div>
         <Transition>
-          <div v-if="isInsertingNewAnnotation" id="new-sticky-container p-2">
+          <div v-if="isInsertingNewAnnotation" id="new-sticky-container" class="p-2 fixed">
             <!-- This is for new annotation stickes -->
-            <StickyNote 
-              :isNew="isInsertingNewAnnotation" 
-              :color="newStickyData?.color!" 
-              :pinNumber="newStickyData?.pinNumber!" 
-              :documentId="newStickyData?.documentId!" 
-              :title="newStickyData?.title"
-              :onUpdateCreate="handleUpdateCreateSticky"
-              :onCancel="handleCloseOutSticky"
-              :uuid="newStickyData?.uuid"
-            />
+            <StickyNote :isNew="isInsertingNewAnnotation" :color="newStickyData?.color!"
+              :pinNumber="newStickyData?.pinNumber!" :documentId="newStickyData?.documentId!"
+              :title="newStickyData?.title" :onUpdateCreate="handleUpdateCreateSticky" :onCancel="handleCloseOutSticky"
+              :uuid="newStickyData?.uuid" />
           </div>
         </Transition>
-        <div class="flex flex-wrap">
+        <div class="flex flex-wrap fixed" v-if="!isInsertingNewAnnotation">
           <!-- This is rendering the existing stickies in readonly mode, but can be edited if they click the menu option -->
           <TransitionGroup>
-            <StickyNote 
-              v-for="sticky in stickies" :key="sticky.sticky_id"
-              :stickyData="sticky" 
-              :color="sticky.color" 
-              :pinNumber="sticky.anchor" 
-              :documentId="sticky.document_id" 
-              :readonly="true"
-              :uuid="sticky.sticky_id"
-              :canEdit="true"
-              :onUpdateCreate="handleUpdateCreateSticky"
-            />
+            <StickyNote v-for="sticky in stickiesInView" :key="sticky.sticky_id" :stickyData="sticky"
+              :color="sticky.color" :pinNumber="sticky.anchor" :documentId="sticky.document_id" :readonly="true"
+              :uuid="sticky.sticky_id" :canEdit="true" :onUpdateCreate="handleUpdateCreateSticky" />
           </TransitionGroup>
         </div>
       </div>
     </div>
-    <ConfirmationModal
-      prompt="Are you sure you want to delete this document?"
-      :open="confirmDeleteWindowOpen" 
-      :onClose="toggleConfirmDeleteWindow" 
-      :onConfirmAction="handleDeleteDocument" 
-    />
+    <ConfirmationModal prompt="Are you sure you want to delete this document?" :open="confirmDeleteWindowOpen"
+      :onClose="toggleConfirmDeleteWindow" :onConfirmAction="handleDeleteDocument" />
   </div>
 </template>
 
 <script setup lang="ts">
-import { isEqual } from 'lodash';
+import { isEmpty, isEqual } from 'lodash';
 import { BlockAnnoteMarkerReconciler } from '~/app/utils/block-annote-marker-reconciler/block-annote-marker-reconciler';
-import type { AnyBlockType, EditorJsBlock } from '~/types/annote-document/editjs-block';
+import type { EditorJsBlock } from '~/types/annote-document/editjs-block';
 import type { ActionType } from '~/types/sticky/action-type/action-type';
 import type { StickyCreateActionData, StickyUpdateActionData } from '~/types/sticky/sticky-action-data/sticky-action-data';
 import type { LinkSticky, Sticky, VideoSticky } from '~/types/sticky/sticky-types';
 import type { AnnoteOnMarkerInsertedData, AnnotteOnMarkerDeletedData } from '../../../../utils/annote-marker/definitions/types';
 
-const annoteDocument = ref<AnnoteDocument | null>(null);
+const route = useRoute();
+const { deleteDocument, isMarkerInViewPort } = useDocument();
+const { fetchStickies } = useSticky();
+const router = useRouter();
 
+const annoteDocument = ref<AnnoteDocument | null>(null);
 // The comparison document is going to be used to determine if there are changes in the document
 const annoteComparisonDocument = ref<AnnoteDocument | null>(null);
 
@@ -82,19 +66,41 @@ const documentTitle = ref("");
 const editorController = ref<CustomEditorJs | null>(null);
 
 const initialDocumentTitle = ref(""); // The original title when the document loads. This will not change.
-const route = useRoute();
 
 const isInsertingNewAnnotation = ref(false);
 const newStickyData = ref<{ pinNumber: number; color: string; title: string, documentId: string, uuid: string } | null>(null);
 
 const stickies = ref<Sticky[]>([]);
-const { fetchStickies } = useSticky();
+const stickiesInView = ref<Sticky[]>([]);
+
 
 const confirmDeleteWindowOpen = ref(false);
-const { deleteDocument } = useDocument();
-const router = useRouter();
 
 const { id } = route.query;
+
+onMounted(() => {
+  window.addEventListener("scroll", handleWindowScroll);
+
+  // This is hacky - but we need to wait for DOM to be ready before we can check for markers in viewport on initial load
+
+  setTimeout(() => {
+    handleWindowScroll();
+  }, 100);
+
+});
+
+function handleWindowScroll() {
+  const markers = document.getElementsByClassName(ANNOTE_MARKER_CSS);
+  const uuidsInViewPort: string[] = [];
+
+  for (let i = 0; i < markers.length; i++) {
+    if (markers[i] && isMarkerInViewPort(markers[i] as HTMLElement)) {
+      uuidsInViewPort.push((markers[i] as any)?.dataset.uuid);
+    }
+  }
+
+  stickiesInView.value = stickies.value.filter((sticky) => uuidsInViewPort.includes(sticky.sticky_id));
+}
 
 async function handleDocumentTitleBlur() {
   // This function handles when the user clicks out of the title input field
@@ -103,8 +109,10 @@ async function handleDocumentTitleBlur() {
   // TODO: Handle the error when the document title is empty
 
   if (documentTitle.value === initialDocumentTitle.value) return;
-
-  if (documentTitle.value.trim() === "") return;
+  if (documentTitle.value.trim() === "") {
+    documentTitle.value = initialDocumentTitle.value;
+    return;
+  }
 
   const { data: apiResponse } = await useFetch<ApiResponse<AnnoteDocument>>(
     `/api/annote_documents/${id}`,
@@ -126,8 +134,10 @@ async function handleDocumentTitleBlur() {
 }
 
 async function patchAnnoteDocumentBlocks(): Promise<AnnoteDocument> {
-  const blockData = await editorController.value?.save();
+  const snapshot = await editorController.value?.save();
 
+  // Reconcile the blocks to ensure that the pin numbers are in order
+  const blockData = new BlockAnnoteMarkerReconciler().reconcile(snapshot?.blocks as EditorJsBlock[]);
   const { data: apiResponse } = await useFetch<ApiResponse<AnnoteDocument>>(
     `/api/annote_documents/${id}`,
     {
@@ -135,6 +145,7 @@ async function patchAnnoteDocumentBlocks(): Promise<AnnoteDocument> {
       body: { blocks: blockData?.blocks },
     }
   );
+
   return apiResponse.value?.data!;
 }
 
@@ -151,7 +162,7 @@ if (id) {
   stickies.value = await fetchStickies(id as string);
 }
 
-function handleMarkerInserted (data?: AnnoteOnMarkerInsertedData) {
+function handleMarkerInserted(data?: AnnoteOnMarkerInsertedData) {
   if (!data) {
     console.warn("No data was returned. Aborting");
     return;
@@ -165,36 +176,50 @@ function handleEditorReady(editor: CustomEditorJs) {
   editorController.value = editor;
   editor.isReady.then(() => {
     editor.render({
-      blocks: annoteDocument.value?.blocks as any,  
+      blocks: annoteDocument.value?.blocks as any,
     });
   });
 }
 
-async function handleUpdateCreateSticky(action: ActionType, values: StickyCreateActionData | StickyUpdateActionData): Promise<void> {
+async function handleUpdateCreateSticky(
+  action: ActionType,
+  values: StickyCreateActionData | StickyUpdateActionData): Promise<void> {
   const { sticky_id } = (values as StickyCreateActionData);
-  const { document_id, title, body, anchor, color, sticky_type, source_url } = values;
-  const httpBody = { document_id, title, body, color, anchor, sticky_type, sticky_id, source_url };
+  const {
+    document_id,
+    title,
+    body,
+    anchor,
+    color,
+    sticky_type,
+    source_url } = values;
+
+  const requestBody = { document_id, title, body, color, anchor, sticky_type, sticky_id, source_url };
   const endPoint = action === "create" ? "/api/sticky" : `/api/sticky/${sticky_id}`;
 
   await useFetch<ApiResponse<Sticky | VideoSticky | LinkSticky>>(
     endPoint,
     {
       method: action === "create" ? "POST" : "PATCH",
-      body: httpBody
+      body: requestBody
     }
   );
-  
+
   await syncAnnoteDocumentData();
 
   stickies.value = await fetchStickies(id as string);
   isInsertingNewAnnotation.value = false;
   newStickyData.value = null;
+
+  // We need to focus on the sticky if it's in view.
+
+  handleWindowScroll();
 }
 
-function handleCloseOutSticky () {
+function handleCloseOutSticky() {
   isInsertingNewAnnotation.value = false;
   newStickyData.value = null;
-  
+
   // This should re-render the editor to the last fetched-from-API state, undoing the new marker. TODO: It's hacky
   editorController.value?.render({
     blocks: annoteDocument.value?.blocks as any,
@@ -205,63 +230,14 @@ function toggleConfirmDeleteWindow() {
   confirmDeleteWindowOpen.value = !confirmDeleteWindowOpen.value;
 }
 
-async function handleDeleteSticky (sticky_id: string) {
-  await useFetch<ApiResponse<Sticky>>(
-    `/api/sticky/${sticky_id}`,
-    {
-      method: "DELETE",
-    }
-  );
-  
-  await syncAnnoteDocumentData();
-  
-  stickies.value = await fetchStickies(id as string);
-}
-
 async function handleEditorLostFocus() {
   // This function handles the click away event from the editor and sends the patch request to
   const newBlockData = await editorController.value?.save();
   const oldBlockData = annoteComparisonDocument.value?.blocks;
 
-  console.log("onBlur newBlockData:", newBlockData)
-
-  const reconciler = new BlockAnnoteMarkerReconciler();
-  const response = reconciler.reconcile(newBlockData?.blocks as EditorJsBlock[]);
-  console.log(response)
   if (isEqual(newBlockData?.blocks, oldBlockData)) return;
 
   await syncAnnoteDocumentData();
-
-  // Here we can reconcile any orphaned stickies
-  // TODO: Fix await reconcileStickies();
-}
-
-async function reconcileStickies (): Promise<void> {
-  // Take the current state of the document and current state of the stickies, use the sticky IDs to determine if there are any stickies that are not in the document
-
-  // Create an array of stickyIds
-  const currentStickyIds = stickies.value.map(sticky => sticky.sticky_id);
-
-  // Convert the blocks data to an array of text data
-  const blocksTextData = annoteDocument.value?.blocks.reduce((acc, block) => {
-    if ("text" in block.data) {
-      acc.push((block.data as AnyBlockType).text);
-    }
-  
-    return acc;
-  }, [] as string[]);
-  
-  const nonExistentStickyIds: string[] = [];
-  currentStickyIds.forEach((stickyId) => {
-    const stickyExists = blocksTextData?.some((blockText) => blockText.includes(stickyId));
-    if (!stickyExists) {
-      nonExistentStickyIds.push(stickyId);
-    }
-  });
-
-  // Send API request to delete the stickies
-  await Promise.allSettled(nonExistentStickyIds.map((stickyId) => handleDeleteSticky(stickyId)));
-  
 }
 
 async function handleDeleteMarker(markerData: AnnotteOnMarkerDeletedData) {
@@ -273,23 +249,51 @@ async function handleDeleteMarker(markerData: AnnotteOnMarkerDeletedData) {
       method: "DELETE",
     }
   );
- 
+
   await syncAnnoteDocumentData();
-  
+
   stickies.value = await fetchStickies(id as string);
+
+  handleWindowScroll();
 }
 
-async function syncAnnoteDocumentData () {
+async function syncAnnoteDocumentData() {
   annoteDocument.value = await patchAnnoteDocumentBlocks();
   annoteComparisonDocument.value = annoteDocument.value;
+
+  await updateStickyMarkerNumbers(new BlockAnnoteMarkerReconciler()
+    .reconcile(annoteDocument.value?.blocks as EditorJsBlock[])
+    .map
+  );
+  stickies.value = await fetchStickies(id as string);
+
+  // Reconcile and sync-up
+  await editorController.value?.render({
+    blocks: annoteDocument.value?.blocks as any,
+  });
 }
 
-async function handleDeleteDocument () {
+async function updateStickyMarkerNumbers(data: Record<string, number>): Promise<void> {
+  // Creates and sends request to update the sticky numbers by id in the database
+  if (isEmpty(data)) return;
+  const promises = Object.entries(data).map(([stickyId, pinNumber]) => {
+    return useFetch<ApiResponse<Sticky>>(
+      `/api/sticky/${stickyId}`,
+      {
+        method: "PATCH",
+        body: { anchor: pinNumber, document_id: id },
+      }
+    );
+  });
+  await Promise.allSettled(promises);
+}
+
+async function handleDeleteDocument() {
   if (!annoteDocument.value) {
     console.error("We can't delete this document. It's probably null.", annoteDocument.value);
     return;
   }
-  
+
   const res = await deleteDocument(annoteDocument.value.document_id);
 
   if (res?.status === "ok") {
