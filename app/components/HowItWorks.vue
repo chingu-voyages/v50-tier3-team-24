fea2ddace4353081683ebc7bc6129cd0b2bd5cda
David Eastmond <davebuck1981@gmail.com>
2026-04-21 12:44:46 -0400

    fix: standardize class usage and improve landing page styles

--- app/components/HowItWorks.vue
+++ app/components/HowItWorks.vue
@@ -1,7 +1,7 @@
 <template>
   <section class="py-16">
     <div class="container px-4 mx-auto">
-      <h2 class="mb-16 text-4xl font-bold text-center text-gray-800">
+      <h2 class="mb-16 text-4xl font-bold text-center app-text">
         Simplify Your Workflow with Annote
       </h2>
       <div class="space-y-24">
@@ -10,8 +10,7 @@
           <div class="mb-8 md:w-1/2 md:mb-0 md:pr-8">
             <div class="flex items-center mb-4">
               <div
-                class="flex items-center justify-center w-12 h-12 mr-4 text-xl font-bold text-white bg-purple-600 rounded-full"
-              >
+                class="flex items-center justify-center w-12 h-12 mr-4 text-xl font-bold text-white bg-purple-600 rounded-full">
                 1
               </div>
               <h3 class="text-2xl font-semibold text-purple-800">
@@ -27,8 +26,7 @@
             <img
               src="https://res.cloudinary.com/dgxqotorm/image/upload/v1723411677/Screenshot_2024-08-11_at_5.26.52_PM_kentbh.png"
               alt="Instant Web Import"
-              class="w-full transition-transform duration-300 transform rounded-lg shadow-lg rotate-2 hover:rotate-0"
-            />
+              class="w-full transition-transform duration-300 transform rounded-lg shadow-lg rotate-2 hover:rotate-0" />
           </div>
         </div>
 
@@ -37,8 +35,7 @@
           <div class="mb-8 md:w-1/2 md:mb-0 md:pl-8">
             <div class="flex items-center mb-4">
               <div
-                class="flex items-center justify-center w-12 h-12 mr-4 text-xl font-bold text-white bg-yellow-600 rounded-full"
-              >
+                class="flex items-center justify-center w-12 h-12 mr-4 text-xl font-bold text-white bg-yellow-600 rounded-full">
                 2
               </div>
               <h3 class="text-2xl font-semibold text-yellow-700">
@@ -54,8 +51,7 @@
             <img
               src="https://res.cloudinary.com/dgxqotorm/image/upload/v1723411483/Screenshot_2024-08-11_at_5.23.27_PM_k6zdyd.png"
               alt="Smart Content Library"
-              class="w-full transition-transform duration-300 transform rounded-lg shadow-lg rotate-neg-2 hover:rotate-0"
-            />
+              class="w-full transition-transform duration-300 transform rounded-lg shadow-lg rotate-neg-2 hover:rotate-0" />
           </div>
         </div>
 
@@ -64,8 +60,7 @@
           <div class="mb-8 md:w-1/2 md:mb-0 md:pr-8">
             <div class="flex items-center mb-4">
               <div
-                class="flex items-center justify-center w-12 h-12 mr-4 text-xl font-bold text-white bg-pink-600 rounded-full"
-              >
+                class="flex items-center justify-center w-12 h-12 mr-4 text-xl font-bold text-white bg-pink-600 rounded-full">
                 3
               </div>
               <h3 class="text-2xl font-semibold text-pink-700">
@@ -81,8 +76,7 @@
             <img
               src="https://res.cloudinary.com/dgxqotorm/image/upload/v1723228093/Screenshot_2024-08-09_at_2.20.45_PM_vt81sw.png"
               alt="Dynamic Document Enhancement"
-              class="w-full transition-transform duration-300 transform rounded-lg shadow-lg rotate-2 hover:rotate-0"
-            />
+              class="w-full transition-transform duration-300 transform rounded-lg shadow-lg rotate-2 hover:rotate-0" />
           </div>
         </div>
       </div>
